2ee19edaced8d1d2b8dd39a87787ef00055832f1
TecPixD <TecPixD@users.noreply.github.com>
2021-08-02 22:08:14 -0300

    add event.repository

--- lib/repositories/event.repository.dart
+++ lib/repositories/event.repository.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tcc_bora_show/models/event.model.dart';
+import 'package:tcc_bora_show/view-models/event.viewmodel.dart';
 
 class EventRepository {
   final _reference = FirebaseFirestore.instance.collection("event");
@@ -8,6 +9,25 @@ class EventRepository {
     try {
       final refs = await _reference.add(model.toMap());
       return refs.id;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<List<EventViewModel>> selectAllEvents() async {
+    try{
+      List<EventViewModel> list = [];
+
+      final snapshots = await _reference.get();
+
+      snapshots.docs.forEach((document) {
+        var eventMap = document.data();
+        eventMap['id'] = document.id;
+        var event = EventViewModel.fromMap(eventMap);
+        list.add(event);
+      });
+
+      return list;
     } catch (e) {
       throw e;
     }
