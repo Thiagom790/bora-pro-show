af57d73691c20e02634dc416523ae90943791848
Thiago Marques <thiagom790@gmail.com>
2021-12-03 20:47:17 -0300

    change profile view

--- lib/views/musician.profile.view.dart
+++ lib/views/musician.profile.view.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:tcc_bora_show/controllers/auth.controller.dart';
+import 'package:tcc_bora_show/controllers/event.controller.dart';
 import 'package:tcc_bora_show/controllers/profile.controller.dart';
 import 'package:tcc_bora_show/core/app.colors.dart';
 import 'package:tcc_bora_show/models/profile.model.dart';
 import 'package:tcc_bora_show/store/auth.store.dart';
 import 'package:tcc_bora_show/store/profile.store.dart';
+import 'package:tcc_bora_show/view-models/management.event.viewmodel.dart';
 import 'package:tcc_bora_show/view-models/profile.viewmodel.dart';
 import 'package:tcc_bora_show/views/create.post.view.dart';
+import 'package:tcc_bora_show/views/event.detail.musician.view.dart';
 import 'package:tcc_bora_show/widgets/error.custom.widger.dart';
 import 'package:tcc_bora_show/widgets/event.card.musician.widget.dart';
+import 'package:tcc_bora_show/widgets/input.widget.dart';
 import 'package:tcc_bora_show/widgets/loading.widget.dart';
 import 'package:tcc_bora_show/widgets/musician.summary.widget.dart';
 import 'package:tcc_bora_show/widgets/post.widget.dart';
@@ -34,6 +38,8 @@ class _MusicianProfileViewState extends State<MusicianProfileView> {
   List<ProfileViewModel> _profiles = [];
   final _authController = AuthController();
   late AuthStore _authStore;
+  final _eventController = EventController();
+  List<ManagementEventViewModel> _listEvents = [];
 
   Future<void> _selectProfile() async {
     try {
@@ -41,6 +47,52 @@ class _MusicianProfileViewState extends State<MusicianProfileView> {
     } catch (e) {
       throw e;
     }
+  }
+
+  Future<void> _selectListEvents() async {
+    try {
+      final musicianID = _profileStore.id;
+      final listEvents = await _eventController.selectMusicianEvent(musicianID);
+
+      this._listEvents = listEvents;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> _loadScreen() async {
+    try {
+      await _selectListEvents();
+      await _selectProfile();
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  List<Widget> get listEventsWidget {
+    List<Widget> list = [];
+    final events = this._listEvents;
+
+    if (events.isEmpty) {
+      list.add(EventCardMusician(title: "Sem Eventos"));
+      return list;
+    }
+
+    list = events.map<Widget>((event) {
+      return EventCardMusician(
+        title: event.title,
+        onPress: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EventDetailMusicianView(eventID: event.id),
+            ),
+          );
+        },
+      );
+    }).toList();
+
+    return list;
   }
 
   //novas funções a partir daqui
@@ -117,7 +169,7 @@ class _MusicianProfileViewState extends State<MusicianProfileView> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<void>(
-      future: _selectProfile(),
+      future: _loadScreen(),
       builder: (context, snapshot) {
         if (ConnectionState.none == snapshot.connectionState ||
             ConnectionState.waiting == snapshot.connectionState) {
@@ -130,134 +182,94 @@ class _MusicianProfileViewState extends State<MusicianProfileView> {
         }
 
         return SingleChildScrollView(
+          padding: EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    ProfilePopupMenuWidget(
-                      profiles: this._profiles,
-                      defaultProfile: this._defaultProfile,
-                      onSelect: this._onSelectProfile,
-                      onCreateProfile: this._createNewProfile,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ProfilePopupMenuWidget(
+                    profiles: this._profiles,
+                    defaultProfile: this._defaultProfile,
+                    onSelect: this._onSelectProfile,
+                    onCreateProfile: this._createNewProfile,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.logout,
+                      color: AppColors.textLight,
                     ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.logout,
-                        color: AppColors.textLight,
-                      ),
-                      onPressed: this._logout,
-                    ),
-                  ],
-                ),
+                    onPressed: this._logout,
+                  ),
+                ],
               ),
               MusicianSummaryWidget(profileModel: profileModel),
               Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Text("Últimos Eventos",
-                    style:
-                        TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: <Widget>[
-                          EventCardMusician(
-                            title: "Sem eventos",
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Últimos Eventos",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: AppColors.container,
-                          border: Border.all(
-                            color: Colors.white,
-                          ),
-                        ),
-                        child: TextFormField(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => createpostview()),
-                            );
-                          },
-                          readOnly: true,
-                          enabled: true,
-                          initialValue: "No que você está pensando?",
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                            contentPadding: EdgeInsets.only(left: 20),
-                          ),
-                          style: TextStyle(
-                            color: AppColors.textLight,
-                          ),
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 12, 0, 5),
-                    child: Text(
-                      "Postagens",
-                      textAlign: TextAlign.start,
-                      style:
-                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Postwidget(
-                    profileName: "Thiago",
-                    postTime: DateTime.now(),
-                    postText:
-                        "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
-                    likeNumber: 999,
-                    commentNumber: 224,
-                    profileOnTap: () {},
-                    likeOnTap: () {},
-                    commentOnTap: () {},
-                  ),
-                  Postwidget(
-                    profileName: "Thiago",
-                    postTime: DateTime.now(),
-                    postText:
-                        "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
-                    likeNumber: 999,
-                    commentNumber: 224,
-                    profileOnTap: () {},
-                    likeOnTap: () {},
-                    commentOnTap: () {},
-                  ),
-                  Postwidget(
-                    profileName: "Thiago",
-                    postTime: DateTime.now(),
-                    postText:
-                        "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
-                    likeNumber: 999,
-                    commentNumber: 224,
-                    profileOnTap: () {},
-                    likeOnTap: () {},
-                    commentOnTap: () {},
-                  ),
-                ],
+              SingleChildScrollView(
+                padding: EdgeInsets.only(bottom: 10),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: listEventsWidget,
+                ),
+              ),
+              InputWidget(
+                placeholder: "No que você está pensando?",
+                readOnly: true,
+                borderColor: AppColors.textLight,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => createpostview()),
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  "Postagens",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Postwidget(
+                profileName: "Thiago",
+                postTime: DateTime.now(),
+                postText:
+                    "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
+                likeNumber: 999,
+                commentNumber: 224,
+                profileOnTap: () {},
+                likeOnTap: () {},
+                commentOnTap: () {},
+              ),
+              Postwidget(
+                profileName: "Thiago",
+                postTime: DateTime.now(),
+                postText:
+                    "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
+                likeNumber: 999,
+                commentNumber: 224,
+                profileOnTap: () {},
+                likeOnTap: () {},
+                commentOnTap: () {},
+              ),
+              Postwidget(
+                profileName: "Thiago",
+                postTime: DateTime.now(),
+                postText:
+                    "Musica Nova Lançada! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elementum bibendum risus nec lacinia. In hac habitasse platea dictumst. Mauris eget mi non ligula fringilla sodales non ac augue. Quisque vel consectetur odio. Vivamus scelerisque ex sit amet egestas tempus. Etiam vulputate, metus non dignissim rhoncus, nibh ",
+                likeNumber: 999,
+                commentNumber: 224,
+                profileOnTap: () {},
+                likeOnTap: () {},
+                commentOnTap: () {},
               )
             ],
           ),
