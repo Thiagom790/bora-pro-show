25ae3916945bdd1b9d83c205bf03ccd5c6e9a60a
Thiago Marques <thiagom790@gmail.com>
2021-12-03 21:15:35 -0300

    add post model and repository

---
+++ lib/models/post.model.dart
@@ -0,0 +1,50 @@
+class PostModel {
+  late String id;
+  late String musicianName;
+  late int _dateTime;
+  late String text;
+  late int numLikes;
+  late String pathPhoto;
+
+  PostModel({
+    this.id = "",
+    this.musicianName = "",
+    this.text = "",
+    this.numLikes = 0,
+    this.pathPhoto = "",
+    DateTime? dateTime,
+  }) : _dateTime = dateTime != null
+            ? dateTime.millisecondsSinceEpoch
+            : DateTime.now().millisecondsSinceEpoch;
+
+  DateTime get date {
+    return DateTime.fromMillisecondsSinceEpoch(this._dateTime);
+  }
+
+  set date(DateTime date) {
+    final hour = this.date.hour;
+    final minute = this.date.minute;
+    final newDate = DateTime(date.year, date.month, date.day, hour, minute);
+    this._dateTime = newDate.millisecondsSinceEpoch;
+  }
+
+  PostModel.fromMap(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.musicianName = map['musicianName'];
+    this._dateTime = map['dateTime'];
+    this.text = map['text'];
+    this.numLikes = map['numLikes'];
+    this.pathPhoto = map['pathPhoto'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'musicianName': this.musicianName,
+      'dateTime': this._dateTime,
+      'text': this.text,
+      'numLikes': this.numLikes,
+      'pathPhoto': this.pathPhoto,
+    };
+  }
+}
---
+++ lib/repositories/post.repository.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:tcc_bora_show/models/post.model.dart';
+
+class PostRepositoryDart {
+  final _reference = FirebaseFirestore.instance.collection('posts');
+
+  Future<String> addPost(PostModel post) async {
+    try {
+      final refs = await _reference.add(post.toMap());
+      return refs.id;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
